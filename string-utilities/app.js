0a3775c8de1089a92ed7958ea21e05760cd23179
Gayathri Ravada <gayathri.ravada7@gmail.com>
2026-06-23 18:39:06 -0500

    stringutils

--- string-utilities/app.js
+++ string-utilities/app.js
@@ -1,24 +1,21 @@
 const stringUtils = require('./stringUtils');
-console.log(stringUtils.capitalize("gayathri"));
+
+console.log(stringUtils.capitalize("launchcode"));
 console.log(stringUtils.capitalize(""));
-console.log(stringUtils.capitalize(null));
-console.log(stringUtils.capitalize(" jay"));
+console.log(stringUtils.capitalize("  javascript"));
 
 console.log(stringUtils.reverse(""));
-console.log(stringUtils.reverse("Hello World"));
-console.log(stringUtils.reverse(" Hello "));
+console.log(stringUtils.reverse("launchcode"));
+console.log(stringUtils.reverse("  javascript"));
+console.log(stringUtils.reverse(12345));
 console.log(stringUtils.reverse("12345"));
-console.log(stringUtils.reverse(null));
-console.log(stringUtils.reverse("A1B2C3"));
+console.log(stringUtils.reverse(undefined));
 
-console.log(stringUtils.contains("Hello World","World"));
-console.log(stringUtils.contains("Hello World",""));
-console.log(stringUtils.contains("Hello World","bye"));
-console.log(stringUtils.contains("","World"));
-console.log(stringUtils.contains("",""));
-console.log(stringUtils.contains("Hello World","world"));
-console.log(stringUtils.contains(" Hello","Hell"));
-console.log(stringUtils.contains(null,"World"));
+console.log(stringUtils.contains("", "hello"));
+console.log(stringUtils.contains("hello world", "hello"));
+console.log(stringUtils.contains("hello world", "bye"));
+console.log(stringUtils.contains(null, "hello"));
+console.log(stringUtils.contains("  hello world", "hello"));
 
 
 
@@ -31,5 +28,8 @@ console.log(stringUtils.contains(null,"World"));
 
 
 
- 
+
+
+
+
 
--- string-utilities/stringUtils.js
+++ string-utilities/stringUtils.js
@@ -1,23 +1,22 @@
-function capitalize(str) {
-    if (typeof str !== "string") return "";
+function capitalize(str){
+    if(typeof str !== "string") return "";
+    
+    str = str.trim();
 
-    str = str.trim(); 
-
-    if (str.length === 0) return "";
+    if(str.length === 0) return "";
 
     return str[0].toUpperCase() + str.slice(1).toLowerCase();
+       
 }
 
-function reverse(str) {
-    if (typeof str !== "string") return "";
+function reverse(str){
+    if(typeof str !== "string") return "";
 
     return [...str].reverse().join('');
 }
 
-
-
-function contains(str, substr) {
-    if (typeof str !== "string" || typeof substr !== "string") {
+function contains(str, substr){
+    if (typeof str !== "string" || typeof substr !== "string"){
         return false;
     }
 
